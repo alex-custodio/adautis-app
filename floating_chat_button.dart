25f108269a999e6a3bf88d2b188ea41f4bb041a2
emilyfbez <102989824+emilyfbez@users.noreply.github.com>
2023-06-08 16:27:49 -0300

    Add files via upload

---
+++ floating_chat_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../screens/chat/chat_screen.dart';
+
+class ChatButton extends StatelessWidget {
+  const ChatButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+        backgroundColor: Color(0xFF9932CC),
+        onPressed: (){
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => ChatPage()));
+        },
+        child: const Icon(Icons.chat_outlined),
+      );
+  }
+}
